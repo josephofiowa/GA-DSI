c07949d96720bafe091fe335ce7e69d6d7706226
Joseph Nelson <joseph.nelson2012@gmail.com>
2016-06-17 07:19:22 -0400

    Add files via upload

---
+++ example-lessons/Intro-to-forecasting/ARIMA_2.py
@@ -0,0 +1,89 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 17 02:16:40 2016
+
+@author: JosephNelson
+"""
+
+
+# REQUIRES DEV VERSION OF STATSMODELS
+# git clone git://github.com/statsmodels/statsmodels.git
+# python setup.py install
+
+import pandas as pd
+%matplotlib inline
+
+df = pd.read_csv('https://raw.githubusercontent.com/generalassembly-studio/dsi-course-materials/master/curriculum/04-lessons/week-09/4.1-lesson/assets/data/euretail.csv?token=ANUte7N9Da2tUY5ZVbijhXTKBGlXKDQMks5XbM_EwA%3D%3D')
+df = df.set_index(['Year'])
+df.head()
+df.stack().plot()
+
+# define Dickey-Fuller test
+from statsmodels.tsa.stattools import adfuller
+def test_stationarity(timeseries):
+
+    #Determing rolling statistics
+    rolmean = pd.rolling_mean(timeseries, window=12)
+    rolstd = pd.rolling_std(timeseries, window=12)
+
+    #Plot rolling statistics:
+    fig = plt.figure(figsize=(12, 8))
+    orig = plt.plot(timeseries, color='blue',label='Original')
+    mean = plt.plot(rolmean, color='red', label='Rolling Mean')
+    std = plt.plot(rolstd, color='black', label = 'Rolling Std')
+    plt.legend(loc='best')
+    plt.title('Rolling Mean & Standard Deviation')
+    plt.show()
+    
+    #Perform Dickey-Fuller test:
+    print 'Results of Dickey-Fuller Test:'
+    dftest = adfuller(timeseries, autolag='AIC')
+    dfoutput = pd.Series(dftest[0:4], index=['Test Statistic','p-value','#Lags Used','Number of Observations Used'])
+    for key,value in dftest[4].items():
+        dfoutput['Critical Value (%s)'%key] = value
+    print dfoutput 
+
+from statsmodels.graphics.tsaplots import plot_acf, plot_pacf
+
+# regular diff
+diff0 = df.stack().diff(periods=4)[4:]
+diff0.plot(title='European Retail Trade Differenced')
+plot_acf(diff0, lags=30)
+plot_pacf(diff0, lags=30)
+
+test_stationarity(diff0)
+
+# additional diff
+diff1 = diff0.diff()[1:]
+diff1.plot(title='European Retail Trade Differenced Twice')
+plot_acf(diff1, lags=30)
+plot_pacf(diff1, lags=30)
+
+test_stationarity(diff1)
+
+import statsmodels.api as sm
+data = df.stack().values
+model = sm.tsa.statespace.SARIMAX(data, order=(0,1,1), seasonal_order=(0,1,1,4))
+results = model.fit()
+print results.summary()
+
+# Don't plot the first 5 values, to account for data loss when differencing (d=1 + D=5)
+residuals = results.resid[5:]
+plt.plot(residuals)
+
+plot_acf(residuals, lags=30)
+plot_pacf(residuals, lags=30)
+
+
+model = sm.tsa.statespace.SARIMAX(data, order=(0,1,3), seasonal_order=(0,1,1,4))
+results = model.fit()
+print results.summary()
+
+
+# forecasting
+res = model.fit()
+preds = res.forecast(12)
+fcast = np.concatenate((data, preds), axis=0)
+
+plt.figure();
+plt.plot(data, 'o' , fcast, 'r--');
